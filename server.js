b640f22b1d1f09dc126485d4bb905499023bb60a
Ava Ta-Valladares <27219505@edu.cbe.ab.ca>
2024-10-23 14:57:49 -0600

    working on /gamestate now, added a few things that work now but might not later

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
 const express = require("express");
-const uuid = require("uuid")
+const uuid = require("uuid");
 const server = express();
-server.use(express.json())
-
-
+server.use(express.json());
+server.use(express.static('public'));
 //All your code goes here
 let activeSessions={}
+
+server.get('/newgame', (req,res)=>{
+    let newID = uuid.v4();
+    let newGame ={
+        wordToGuess: "apple",
+        guesses:[],
+        wrongLetters:[],
+        closeLetters:[],
+        rightLetters:[],
+        remainingGuesses:6,
+        gameOver: false
+    }
+    activeSessions[newID] = newGame
+    res.status(201)
+    res.send({sessionID: newID})
+})
+
+// server.get('/gamestate', (req,res)=>{
+//     let sessionID = req.query.sessionID;
+
+//     if (!sessionID){
+//         res.status(400);
+//         return res.send({error: "No session ID providied"});
+//     }
+//     let gamestate = activeSessions[sessionID];
+//     if (!gamestate) {
+//         res.status(404);
+//         return res.send({error: "Game session not found"})
+//     }
+//     let responseState = {...gamestate};
+//     if (!gamestate.gameOver){
+//         wordToGuess = "undefined";
+//     }
+
+//     res.status(200);
+//     res.send({sessionID})
+// })
+
 
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
