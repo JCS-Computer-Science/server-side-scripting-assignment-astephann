c7462d275cfde081af2799ffb20eed376547a75e
Ava Ta-Valladares <27219505@edu.cbe.ab.ca>
2024-10-24 13:31:58 -0600

    finished a test

--- server.js
+++ server.js
@@ -21,36 +21,18 @@ server.get('/newgame', (req,res)=>{
     res.status(201)
     res.send({sessionID: newID})
 })
-server.get('/gamestate', (req,res)=>){
-    const sessionID = req.query.sessionID;
 
-    if (sessionID === 'undefined'){
-        
-    }
-    res.status
-}
+server.get('/gamestate', (req,res)=>{
+    let sessionID = req.query.sessionID;
 
-// server.get('/gamestate', (req,res)=>{
-//     let sessionID = req.query.sessionID;
+    // if (sessionID = undefined){
+    //     res.status(400);
+    //     return res.send({error : "Session ID not found"})
+    // }
 
-//     if (!sessionID){
-//         res.status(400);
-//         return res.send({error: "No session ID providied"});
-//     }
-//     let gamestate = activeSessions[sessionID];
-//     if (!gamestate) {
-//         res.status(404);
-//         return res.send({error: "Game session not found"})
-//     }
-//     let responseState = {...gamestate};
-//     if (!gamestate.gameOver){
-//         wordToGuess = "undefined";
-//     }
-
-//     res.status(200);
-//     res.send({sessionID})
-// })
-
+    res.status(200);
+    res.send(sessionID)
+});
 
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
