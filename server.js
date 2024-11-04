c86e558457712091c99f4bdc4d95ccced38bbd84
Ava Ta-Valladares <27219505@edu.cbe.ab.ca>
2024-11-04 14:25:33 -0700

    was working on it but nothing is working, so I will try a new approach  o the code

--- server.js
+++ server.js
@@ -9,15 +9,31 @@ let activeSessions={}
 
 server.get('/newgame', (req,res)=>{
     let newID = uuid.v4();
+    let answer = (req.query.answer || "hello").toLowerCase();  
     let newGame ={
-        wordToGuess: "apple",
+        wordToGuess: answer,
         guesses:[],
         wrongLetters:[],
+        
         closeLetters:[],
         rightLetters:[],
         remainingGuesses:6,
         gameOver: false
     };
+
+  let guess = (req.query.guess || null)?.toLowerCase();
+
+    if (guess === answer){
+        newGame.gameOver = true;
+    } else if (guess){
+        newGame.guesses.push(guess);
+        newGame.remainingGuesses -= 1;
+        
+        if(newGame.remainingGuesses === 0){
+            newGame.gameOver = true;
+        }
+    }
+
     activeSessions[newID] = newGame;
     res.status(201);
     res.send({sessionID: newID});
@@ -29,6 +45,18 @@ server.get('/gamestate', (req,res) => {
     res.send({gameState: activeSessions[sessionID]});
     res.status(200);
 });
+
+server.post('/guess'), (req,res)=>{
+    let sessionID = req.body.sessionID;
+    let guess = req.body.guess;
+
+    if (sessionID || guess){
+        res.status(404)
+        res.send({error: "Session ID/guess are required"})
+    }
+    
+    res.status(201);
+}
 
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports
